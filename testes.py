c141bf70e11986b3243a67d46a49454d12988102
Larissa Cassanjo <larissacassanjo@gmail.com>
2024-07-03 17:48:18 -0300

    Testando a API TMDB

--- testes.py
+++ testes.py
@@ -1,21 +1,36 @@
-from imdb import Cinemagoer, IMDbError
+import requests #Para conseguir fazer a solicitação HTTP
 
 from random import randint
 from os import system
 system('cls')
+api_key = 'a8b56b86a83dee254fadcaf769bd55dc' #Deixando mais fácil o uso da chave da API
 
-# create an instance of the Cinemagoer class
-ia = Cinemagoer()
+# Função para buscar o ID do gênero pelo nome
+def buscar_genero_id(nome_genero, api_key):
+    url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}&language=pt-BR"
+    response = requests.get(url)
+    data = response.json()
+    for genero in data['genres']:
+        if genero['name'].lower() == nome_genero.lower():
+            return genero['id']
+    return None
 
-#Mostrar pessoa:
-pessoa = ia.search_person('julia roberts')
-print(pessoa[0])
-julia = pessoa[0].personID
-print(julia)
+# Função para buscar filmes por gênero
+def buscar_filmes_por_genero(genero_id, api_key):
+    url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_genres={genero_id}&language=pt-BR"
+    response = requests.get(url)
+    data = response.json()
+    if 'results' in data:
+        return data['results']
+    else:
+        return []
 
-#Procurar filme por gênero
-movies = ia.search_keyword('romance')
-print(len(movies))
-movies = movies[0]
-choice = ia.get_keyword(movies)
-print(len(choice))
+# Exemplo de uso
+nome_genero = "Romance"
+genero_id = buscar_genero_id(nome_genero, api_key)
+if genero_id:
+    filmes = buscar_filmes_por_genero(genero_id, api_key)
+    for filme in filmes:
+        print(f"Titulo: {filme['title']}, Ano: {filme['release_date'][:4]}, TMDb ID: {filme['id']}")
+else:
+    print("Gênero não encontrado.")
